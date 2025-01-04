2ca5723aae2f2f5bd07b1956e6983fb84abf8495
Marvel Collin <marvelcollin7@gmail.com>
2025-01-05 00:59:30 +0700

    feat: add backend rust test

---
+++ backend/target/debug/deps/backend-ebb93bdd3eb3eefa.d
@@ -0,0 +1,5 @@
+C:\BINUS\ASLAB\PLagiaMVision\backend\target\debug\deps\libbackend-ebb93bdd3eb3eefa.rmeta: src/main.rs
+
+C:\BINUS\ASLAB\PLagiaMVision\backend\target\debug\deps\backend-ebb93bdd3eb3eefa.d: src/main.rs
+
+src/main.rs:
